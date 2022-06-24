4d34961fb6b4efa9bc22562b15de941c4a0bad24
vicchuu <vishnumaniplt@gmail.com>
2022-06-24 07:39:11 +0530

    new commit few changes in templates/flask

--- scratch.py
+++ scratch.py
@@ -14,3 +14,22 @@ dic =  {
 
 
 print(sorted(dic.items() , key =  lambda x :x[0] , reverse=False ))
+
+s = dict((x,"Even") if x%2 ==0 else (x,"Odd") for x in range(18))
+print(s)
+
+# dict1={"one":1,"two":2,"three":3}
+# duplicate=[]
+# for a in range(len(dict1)):
+#
+#     sml=[a for a in dict1.values() if a ==min(dict1.values())]
+#     key1=[k for k,v in dict1.items() if v ==min(dict1.values())]
+#     del dict1(key1)
+#     duplicate.append(sml)
+
+#print(sml)
+
+ce = "vishnu is gooooog boy in nature"
+from collections import Counter
+print((Counter(ce)))
+#print(key1)
