4a99091243543d0a459a2fdfa09e8c2178e5152d
vicchuu <vishnumaniplt@gmail.com>
2022-07-02 19:06:43 +0530

    new commit few changes in templates/flask

--- server.py
+++ server.py
@@ -1,17 +1,37 @@
-from flask import Flask ,render_template , url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"]=("sqlite:///testSQL.db")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-@app.route("/" , methods=['GET','POST'])
-def imdex():
-    return   render_template("index.html")
+class finalDB(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    timethis = db.Column(db.DateTime, default=datetime.utcnow)
 
-#pip install -U Flask-SQLAlchemy pip install Flask-SQLAlchemy
-if __name__ =="__main__":
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = finalDB(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = finalDB.query.order_by(finalDB.timethis).first()
+        return render_template('index.html', tasks=tasks)
+
+if __name__ == "__main__":
     app.run(debug=True)
